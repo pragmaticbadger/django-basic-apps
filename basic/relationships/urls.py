86a0832a6c853e1bdd8ba4c27af88679996cc1dd
Daniel Lindsley <daniel@toastdriven.com>
2010-03-23 23:50:53 -0500

    Usernames may contain other characters.

--- basic/relationships/urls.py
+++ basic/relationships/urls.py
@@ -2,27 +2,27 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('basic.relationships.views',
-    url(r'^following/(?P<username>\w+)/$',
+    url(r'^following/(?P<username>[\w\d._-]+)/$',
         view='following',
         name='relationship_following'
     ),
-    url(r'^followers/(?P<username>\w+)/$',
+    url(r'^followers/(?P<username>[\w\d._-]+)/$',
         view='followers',
         name='relationship_followers'
     ),
-    url(r'^follow/(?P<username>\w+)/$',
+    url(r'^follow/(?P<username>[\w\d._-]+)/$',
         view='follow',
         name='relationship_follow'
     ),
-    url(r'^unfollow/(?P<username>\w+)/$',
+    url(r'^unfollow/(?P<username>[\w\d._-]+)/$',
         view='unfollow',
         name='relationship_unfollow'
     ),
-    url(r'^block/(?P<username>\w+)/$',
+    url(r'^block/(?P<username>[\w\d._-]+)/$',
         view='block',
         name='relationship_block'
     ),
-    url(r'^unblock/(?P<username>\w+)/$',
+    url(r'^unblock/(?P<username>[\w\d._-]+)/$',
         view='unblock',
         name='relationship_unblock'
     ),
